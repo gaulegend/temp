a77abb4fab8d53668ab787a28d7dd8529c9e4c96
gaulegend <gaulegend@gmail.com>
2025-06-29 16:33:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,6 @@ import streamlit as st
 st.title('This is title')
 st.header("This is header")
 st.subheader("This is subheader")
+
+items= ['item 1', 'item 2', 'item 3']
+st.selectbox("This is dropdown box", items)
