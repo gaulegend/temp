e9460566c51050f6fc9ba593fd4b30e07a9d67f9
gaulegend <gaulegend@gmail.com>
2025-06-30 19:05:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -76,7 +76,8 @@ df = pd.read_csv("https://raw.githubusercontent.com/hiteshchoudhary/Streamlit-co
 with st.expander("Show Dataframe"):
   st.dataframe(df)
 
-
+if st.button("Show Summary Stats:"):
+  st.write(df.describe())
 
 
 
