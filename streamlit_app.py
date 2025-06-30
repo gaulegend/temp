aac0e3fb27efdc6bee52763040ca713862288b4d
gaulegend <gaulegend@gmail.com>
2025-06-30 19:33:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -81,7 +81,7 @@ with st.expander("Show Dataframe"):
   if show_stats:
     col1, col2 = st.columns(2)
     with col1:
-      st.header("Overall Summary Statistics")
+      st.subheader("Overall Summary Statistics")
       st.write(df.describe())
 
     with col2: 
